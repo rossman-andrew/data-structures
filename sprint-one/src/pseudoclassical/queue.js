1aef77d6e1b65dd9a21912dfd7f000295bafbc60
rossman-andrew <andrew100r@gmail.com>
2017-08-23 21:39:23 -0700

    Implemented pseudoclassical features for queue and stack

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.front = -1;
+  this.end = -1;
+  this.storage = {};  
+};
+
+Queue.prototype.size = function() {
+  if (this.end - this.front < 0 || this.front === -1) {
+    return 0;
+  }
+  return this.end - this.front + 1;
+};
+
+Queue.prototype.enqueue = function(value) {
+  if (this.front === -1) {
+    this.front = 0;
+  }
+  this.end++;
+  var key = this.end;
+  this.storage[key] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  var dequeueKey = this.front;
+  this.front += 1;
+  if (this.size() === 0) {
+    this.front = this.end = -1;
+  }
+  return this.storage[dequeueKey];
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.sizeOf = 0;
+  this.storage = {};
 };
 
+Stack.prototype.size = function() { return this.sizeOf; },
+
+Stack.prototype.push = function(value) { 
+  var key = this.sizeOf;
+  this.sizeOf++;
+  this.storage[key] = value;
+};
+
+Stack.prototype.pop = function() { 
+  if (this.sizeOf === 0) {
+    return 0;
+  }
+  var key = this.sizeOf - 1;
+  this.sizeOf--;
+  return this.storage[key];
+};
 
