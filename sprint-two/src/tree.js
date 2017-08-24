36fda1a9d0245b04510ca8d62e266731880379ac
rossman-andrew <andrew100r@gmail.com>
2017-08-24 11:41:29 -0700

    Implemented Tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,21 +1,38 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = {};
+  newTree.childrenNumber = 0;
+  newTree.contains = treeMethods.contains;
+  newTree.addChild = treeMethods.addChild;
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Object.create(this);
+  child.value = value;
+  child.childrenNumber = 0;
+  child.children = {};
+  
+  this.children[Number(this.childrenNumber++)] = child;  
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target) {  
+  var searchNode = function(node) { 
+    if (node.value === target) {
+      return true;
+    }
+    for (var child in node.children) {
+      if (searchNode(node.children[child])) {
+        return true;
+      }
+    }
+    return false;
+  };
+  return searchNode(this);
 };
-
 
 
 /*
