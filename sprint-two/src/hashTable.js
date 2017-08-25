80f2d3401f0a98d9eb6e505f0530d41f61fb5c6c
rossman-andrew <andrew100r@gmail.com>
2017-08-24 22:03:13 -0700

    Added logic for hashTable on insert and retrieve

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,39 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[index] === undefined) { 
+    this._storage[index] = [];
+  }
+  var keyVal = [k, v];
+  this._storage[index].forEach(function(keyValPairs, i, collection) {
+    if (keyValPairs[0] === k) {
+      collection[i] = keyVal;
+      return;
+    }
+  });
+  this._storage[index].push(keyVal);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  console.log(this._storage[index][0]);
+  console.log(this._storage[index].length);
+  if (this._storage[index].length === 1) {
+    return this._storage[index][0][1];
+  } else {
+    this._storage[index].forEach(function(keyValPairs) {
+      if (keyValPairs[0] === k) {
+        return keyValPairs[1];
+      }
+    });
+  }
+  
+  // return this._storage[index];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  delete this._storage[index];
 };
 
 
