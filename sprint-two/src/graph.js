52f8c0e58323452983419979fb573193758cb952
rossman-andrew <andrew100r@gmail.com>
2017-08-24 20:15:03 -0700

    Implemented graphs and sets data structure

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,38 +2,72 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.vertices = [];
+  this.edges = [];
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.vertices.push(node);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this.vertices.indexOf(node) > -1 ? true : false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  var nodeIndex = this.vertices.indexOf(node);
+  //nodeIndex > -1 ? this.vertices.splice(nodeIndex, 1) : null;
+  if (nodeIndex > -1) {
+    this.vertices.splice(nodeIndex, 1);
+    this.edges.forEach(function(connection, index, collection) {
+      if (connection[0] === node || connection[1] === node) {
+        collection.splice(index, 1);
+      }
+    });
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  var found = false;
+  this.edges.forEach(function(edgeArray) {
+    if (edgeArray[0] === fromNode && edgeArray[1] === toNode || edgeArray[1] === fromNode && edgeArray[0] === toNode) {
+      found = true;
+      return;
+    }
+  });
+  return found;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  var nodeEdges = [fromNode, toNode];
+  this.edges.push(nodeEdges);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  this.edges.forEach(function(edgeArr, index, collection) {
+    if (edgeArr[0] === fromNode && edgeArr[1] === toNode || edgeArr[0] === toNode && edgeArr[1] === fromNode) {
+      collection.splice(index, 1);
+      return;
+    }
+  });
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  this.vertices.forEach(function(nodes) {
+    cb(nodes);
+  });   
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
 
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.indexOf(item) > -1;
 };
 
 setPrototype.remove = function(item) {
+  var index = this._storage.indexOf(item);
+  if (index > -1) {
+    this._storage.splice(index, 1);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ 
+ Add = O(1): constant time
+ Contains = O(n): linear time
+ Remove = O(n): linear time
+ 
  */
