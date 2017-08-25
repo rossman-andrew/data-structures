77ae3b32c13a2e1b9dfd1a43bd2a5f9bcc69c209
rossman-andrew <andrew100r@gmail.com>
2017-08-25 16:15:03 -0700

    Added Big-O time complexity

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -37,4 +37,8 @@ treeMethods.contains = function(target) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ 
+addChild - O(1) - Constant time.
+contains - O(n) - Linear time.
+ 
  */
